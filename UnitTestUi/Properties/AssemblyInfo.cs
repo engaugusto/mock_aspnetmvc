e8c929e0e9baca04a9f48b69cfa792729bebbe73
Carlos Augusto <carlos.augusto@ecommconsultoria.com.br>
2017-11-30 15:03:28 -0200

    First version with mock and aspnet mvc

---
+++ UnitTestUi/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestUi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestUi")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8428511c-51f5-4e3c-b35e-9031619ca0d9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestUi/UnitTest1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MockHttpRequestTest.Controllers;
+using System.Web;
+using Moq;
+using System.Collections.Specialized;
+using System.Web.Mvc;
+using System.IO;
+using System.Web.Routing;
+
+namespace UnitTestUi
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var m = new Mock<HomeController>();
+            
+            var k = new NameValueCollection();
+            k.Add("teste", "1");
+            var contextMocked = MockHttpContext2();
+            contextMocked.Setup(x => x.Request.Form).Returns(k);
+            //m.SetupGet(x => x.HttpContext).Returns(contextMocked.Object);
+
+
+
+            var objController = m.Object;
+            objController.ControllerContext 
+                = new ControllerContext(
+                    contextMocked.Object, 
+                    new RouteData(), 
+                    objController
+                  );
+            objController.Index();
+        }
+
+        public static Mock<HttpContextBase> MockHttpContext2()
+        {
+            var context = new Mock<HttpContextBase>();
+            var request = new Mock<HttpRequestBase>();
+            var response = new Mock<HttpResponseBase>();
+            var session = new Mock<HttpSessionStateBase>();
+            var server = new Mock<HttpServerUtilityBase>();
+
+            request.Setup(r => r.AppRelativeCurrentExecutionFilePath).Returns("/");
+            request.Setup(r => r.ApplicationPath).Returns("/");
+
+            response.Setup(s => s.ApplyAppPathModifier(It.IsAny<string>())).Returns<string>(s => s);
+            response.SetupProperty(res => res.StatusCode, (int)System.Net.HttpStatusCode.OK);
+
+            context.Setup(h => h.Request).Returns(request.Object);
+            context.Setup(h => h.Response).Returns(response.Object);
+
+            context.Setup(ctx => ctx.Request).Returns(request.Object);
+            context.Setup(ctx => ctx.Response).Returns(response.Object);
+            context.Setup(ctx => ctx.Session).Returns(session.Object);
+            context.Setup(ctx => ctx.Server).Returns(server.Object);
+
+            return context;
+        }
+
+        public static HttpContextBase MockHttpContext()
+        {
+            var context = new Mock<HttpContextBase>();
+            var request = new Mock<HttpRequestBase>();
+            var response = new Mock<HttpResponseBase>();
+            var session = new Mock<HttpSessionStateBase>();
+            var server = new Mock<HttpServerUtilityBase>();
+
+            request.Setup(r => r.AppRelativeCurrentExecutionFilePath).Returns("/");
+            request.Setup(r => r.ApplicationPath).Returns("/");
+
+            response.Setup(s => s.ApplyAppPathModifier(It.IsAny<string>())).Returns<string>(s => s);
+            response.SetupProperty(res => res.StatusCode, (int)System.Net.HttpStatusCode.OK);
+
+            context.Setup(h => h.Request).Returns(request.Object);
+            context.Setup(h => h.Response).Returns(response.Object);
+
+            context.Setup(ctx => ctx.Request).Returns(request.Object);
+            context.Setup(ctx => ctx.Response).Returns(response.Object);
+            context.Setup(ctx => ctx.Session).Returns(session.Object);
+            context.Setup(ctx => ctx.Server).Returns(server.Object);
+
+            return context.Object;
+        }
+    }
+}
